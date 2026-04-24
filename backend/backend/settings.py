2084150d7438abb6959c32c1364a5f3cb5b495bb
Luis Vides <lalejandrovides@uniguajira.edu.co>
2026-04-24 18:03:39 -0500

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'iKLLThYNeQxVpWfRrIHTNKvmEPKrlJNi',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': 36086,
     }
 }
 
